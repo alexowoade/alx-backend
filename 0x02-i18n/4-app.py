d1bf64e97e58c025bf2847267a0abc5af2d2ee9f
Obelem <ibbraidejesse@gmail.com>
2023-02-28 15:35:29 +0100

    added template folder

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -13,7 +13,7 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 babel = Babel(app)
 
