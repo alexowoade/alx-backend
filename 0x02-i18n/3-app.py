82eb79b69b2063461e216fcb83f0ca08b67c1c3b
Obelem <ibbraidejesse@gmail.com>
2023-02-28 14:39:25 +0100

    refactored code

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python3
-"""Basic Babel Setup"""
-from flask import Flask, render_template, request
-from flask_babel import Babel
+""" Module for trying out Babel i18n """
+from flask_babel import Babel, _
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
 
 
-class Config:
-    """Config class for flask app
-    """
-    DEBUG = True
+class Config(object):
+    """ Configuration Class for Babel """
+
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
-
-# use this below instead of @babel.localeselector /
-# for modern versions of babel
-# babel.init_app(app, locale_selector=get_locale)
 
 
-@app.route('/')
-def index():
-    """view function for root route
-
-    Returns:
-        html: homepage
-    """
-    return render_template('3-index.html')
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("3-index.html")
 
 
 @babel.localeselector
-def get_locale():
-    """get best language match
-
-    Returns:
-        str: best match
-    """
+def get_locale() -> str:
+    """Select a language translation to use for that request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
