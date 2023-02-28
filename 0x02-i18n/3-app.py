dc3bb164148f2888be393f9db13b623889b9d35b
Obelem <ibbraidejesse@gmail.com>
2023-02-28 13:30:32 +0100

    used @locale decorator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,16 +22,6 @@ babel = Babel(app)
 # babel.init_app(app, locale_selector=get_locale)
 
 
-@babel.localeselector
-def get_locale():
-    """get best language match
-
-    Returns:
-        str: best match
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route('/')
 def index():
     """view function for root route
@@ -40,6 +30,16 @@ def index():
         html: homepage
     """
     return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """get best language match
+
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
