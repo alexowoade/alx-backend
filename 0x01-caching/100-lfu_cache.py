7da1af13072ca428a6eb320bea2a7b84a736dde2
alexowoade <alexowoade@gmail.com>
2023-05-17 15:13:18 +0000

    alex

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,47 +1,41 @@
 #!/usr/bin/python3
-"""LFU Cache Replacement Implementation Class
-"""
-from threading import RLock
-BaseCaching = __import__('base_caching').BaseCaching
-class LFUCache(BaseCaching):
-    """
-    An implementaion of LFUCache(Least frequently used)
-    Attributes:
-        __stats (list): A dictionary of cache keys for access count
-        __rlock (RLock): Lock accessed resources to prevent race condition
-    """
-    def __init__(self):
-        """ Instantiation method, sets instance attributes
-        """
-        super().__init__()
-        self.__stats = {}
-        self.__rlock = RLock()
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key is not None and item is not None:
-            keyOut = self._balance(key)
-            with self.__rlock:
-                self.cache_data.update({key: item})
-            if keyOut is not None:
-                print('DISCARD: {}'.format(keyOut))
-    def get(self, key):
-        """ Get an item by key
-        """
-        with self.__rlock:
-            value = self.cache_data.get(key, None)
-            if key in self.__stats:
-                self.__stats[key] += 1
-        return value
-    def _balance(self, keyIn):
-        """ Removes the earliest item from the cache at MAX size
-        """
-        keyOut = None
-        with self.__rlock:
-            if keyIn not in self.__stats:
-                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                    keyOut = min(self.__stats, key=self.__stats.get)
-                    self.cache_data.pop(keyOut)
-                    self.__stats.pop(keyOut)
-            self.__stats[keyIn] = self.__stats.get(keyIn, 0) + 1
-        return keyOut
+""" 100-main """
+LFUCache = __import__('100-lfu_cache').LFUCache
+
+my_cache = LFUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
+my_cache.put("L", "L")
+my_cache.print_cache()
+my_cache.put("M", "M")
+my_cache.print_cache()
+
