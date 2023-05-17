4b581d04f2583bf918287661f49b80898f90bf1c
alexowoade <alexowoade@gmail.com>
2023-05-17 15:19:35 +0000

    alex

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,41 +1,61 @@
-#!/usr/bin/python3
-""" 100-main """
-LFUCache = __import__('100-lfu_cache').LFUCache
+#!/usr/bin/env python3
+"""
+LFU Cache Module
+"""
+from collections import defaultdict
+from datetime import datetime
+from base_caching import BaseCaching
 
-my_cache = LFUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
-my_cache.put("L", "L")
-my_cache.print_cache()
-my_cache.put("M", "M")
-my_cache.print_cache()
+class LFUCache(BaseCaching):
+    """
+    LFU Cache Class
+    """
+
+    def __init__(self):
+        """
+        Initialize LFU Cache
+        """
+        super().__init__()
+        self.frequency = defaultdict(int)
+        self.last_used = {}
+
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.frequency[key] += 1
+            self.last_used[key] = datetime.now()
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            min_freq = min(self.frequency.values())
+            keys_with_min_freq = [k for k, v in self.frequency.items() if v == min_freq]
+            if len(keys_with_min_freq) == 1:
+                discarded_key = keys_with_min_freq[0]
+            else:
+                discarded_key = min(keys_with_min_freq, key=lambda k: self.last_used[k])
+            del self.cache_data[discarded_key]
+            del self.frequency[discarded_key]
+            del self.last_used[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+        self.frequency[key] = 1
+        self.last_used[key] = datetime.now()
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.frequency[key] += 1
+        self.last_used[key] = datetime.now()
+        return self.cache_data[key]
 
